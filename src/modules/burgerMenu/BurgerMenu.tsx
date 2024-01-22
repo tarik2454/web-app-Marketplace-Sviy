7ec1bd50b38b2f8cbdfecf736b12489aeefde8f9
Taras Kovalyk <tarik2454@gmail.com>
2024-01-22 19:06:58 +0200

    adds focus to card

--- src/modules/burgerMenu/BurgerMenu.tsx
+++ src/modules/burgerMenu/BurgerMenu.tsx
@@ -1,27 +1,29 @@
-"use client"
-import { CatalogueButton, CloseButton }from "@/shared/components/index";
-import { Cabinet } from "./components";
-import { Categories } from "..";
-import { MouseEventHandler, useState } from "react";
+'use client';
+import { CatalogueButton, CloseButton } from '@/shared/components/index';
+import { Cabinet } from './components';
+import { Categories } from '..';
+import { MouseEventHandler, useState } from 'react';
 
 type Props = {
-  display: String,
-  closeButtonClick: MouseEventHandler<HTMLButtonElement>,
-}
+  display: String;
+  closeButtonClick: MouseEventHandler<HTMLButtonElement>;
+};
 
-export default function BurgerMenu ({ display, closeButtonClick }: Props) {
-  const [categoriesDisplay, setCategoriesDisplay] = useState("hidden");
+export default function BurgerMenu({ display, closeButtonClick }: Props) {
+  const [categoriesDisplay, setCategoriesDisplay] = useState('hidden');
   return (
-    <nav className={`${display} fixed z-20 w-full h-screen inset-0 px-4 py-5 overflow-y-auto bg-white`}>
+    <nav
+      className={`${display} fixed z-20 w-full h-screen inset-0 px-4 py-5 overflow-y-auto bg-white`}
+    >
       <CloseButton closeButtonClick={closeButtonClick} />
       <Cabinet />
       <div className="block absolute w-screen left-0 py-5 px-4 mt-5 border-y-2">
-        <CatalogueButton catalogueClick={() => setCategoriesDisplay("block")} />
+        <CatalogueButton catalogueClick={() => setCategoriesDisplay('block')} />
       </div>
-      <Categories 
-        categoriesDisplay={categoriesDisplay} 
-        closeCategoriesClick={() => setCategoriesDisplay("hidden")} 
+      <Categories
+        categoriesDisplay={categoriesDisplay}
+        closeCategoriesClick={() => setCategoriesDisplay('hidden')}
       />
     </nav>
-  )
+  );
 }
