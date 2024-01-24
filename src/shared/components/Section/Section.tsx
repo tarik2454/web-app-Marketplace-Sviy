2dd341b790faf95f8b1912d6f1cae0df1b94ce4a
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-01-24 19:43:55 +0200

    changes in section styles

--- src/shared/components/Section/Section.tsx
+++ src/shared/components/Section/Section.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
 
-export default function Section({ children }: { children: ReactNode }) {
-  return <section className="">{children}</section>;
+export default function Section({ children, className }: { children: ReactNode, className?: string }) {
+  return <section className={className}>{children}</section>;
 }
