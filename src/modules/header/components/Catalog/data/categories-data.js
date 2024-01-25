c61ccaf50a0d928a19c0e8cd059c48d36fb610bc
Taras Kovalyk <tarik2454@gmail.com>
2024-01-25 19:51:22 +0200

    add backdrop to the catalog

---
+++ src/modules/header/components/Catalog/data/categories-data.js
@@ -0,0 +1,21 @@
+const categoriesData = [
+  {
+    id: 0,
+    category: 'Фрукти, ягоди та овочі',
+    subCategory: ['Овочії, Фрукти, Ягоди'],
+  },
+  { id: 1, category: 'Кулінарія' },
+  { id: 2, category: 'Випічка' },
+  { id: 3, category: 'Овочі' },
+  { id: 4, category: 'Фрукти' },
+  { id: 5, category: 'Мед та продукти бджільництва' },
+  { id: 6, category: "М'ясо та м'ясні вироби" },
+  { id: 7, category: 'Молочна продукція' },
+  { id: 8, category: 'Риба та морепродукти' },
+  { id: 9, category: 'Яйця та птиця' },
+  { id: 10, category: 'Зернові та бобові' },
+  { id: 11, category: 'Горіхи та сухофрукти' },
+  { id: 12, category: 'Консервовані продукти' },
+];
+
+export default categoriesData;
