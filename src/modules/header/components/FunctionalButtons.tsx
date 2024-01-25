aeefaac8c27b5e7849ed8f0b1bf5ac35b1d1fa6c
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-01-25 13:04:53 +0200

    changed svg styles, created two new pages

--- src/modules/header/components/FunctionalButtons.tsx
+++ src/modules/header/components/FunctionalButtons.tsx
@@ -10,17 +10,17 @@ type Props = {
 export default function FunctionalButtons ({ searchButtonClick, cartButtonClick }: Props) {
   return (
     <div className="flex gap-4 md:gap-8">
-      <button onClick={searchButtonClick} className="w-8">
+      <button onClick={searchButtonClick} className="w-8 text-blue-700 hover:text-neutral-800 active:text-neutral-400">
         <SpriteSVG name="search" />
       </button>
-      <button className="hidden w-8 md:block">
+      <button className="hidden w-8 md:block text-blue-700 hover:text-neutral-800 active:text-neutral-400">
         <SpriteSVG name="user" />
       </button>
-      <button className="hidden w-8 relative md:block">
+      <button className="hidden w-8 relative md:block text-blue-700 hover:text-neutral-800 active:text-neutral-400">
         <Counter />
         <SpriteSVG name="favorite-header" />
       </button>
-      <button onClick={cartButtonClick} className="relative w-8">
+      <button onClick={cartButtonClick} className="relative w-8 text-blue-700 hover:text-neutral-800 active:text-neutral-400">
         <Counter />
         <SpriteSVG name="cart" />
       </button>
