78a2769759b973db2732957e29d533496ce727bc
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2024-01-26 19:35:31 +0200

    add breadcrumbs start

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,14 @@
-export default function Page () {
+import { Breadcrumbs } from '@/shared/components';
+import { SpriteSVG } from '@/shared/img/SpriteSVG';
+
+export default function Page() {
   return (
     <main className="pt-80">
-      
+      <Breadcrumbs
+        homeElement={<span>Головна</span>}
+        separator={<SpriteSVG name="expand_right" />}
+        capitalizeLinks
+      />
     </main>
-  )
+  );
 }
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -3,4 +3,5 @@ export { default as CatalogButton } from '../../modules/header/components/Catalo
 export { default as CloseButton } from './CloseButton/CloseButton';
 export { default as OrangeButton } from './OrangeButton/OrangeButton';
 export { default as ArrowButton } from './ArrowButton/ArrowButton';
-export { default as FormInput } from "./FormInput/FormInput";
+export { default as FormInput } from './FormInput/FormInput';
+export { default as Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
