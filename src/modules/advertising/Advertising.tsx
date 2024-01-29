2b113cbcbe7e15b08ed1626aeae654cdda25e162
LukeBrutale <yuriybema@gmail.com>
2024-01-29 15:17:51 +0200

    add modal

--- src/modules/advertising/Advertising.tsx
+++ src/modules/advertising/Advertising.tsx
@@ -20,20 +20,35 @@ const Advertising: React.FC<AdvertisingProps> = ({
     <Section>
       <Container>
         <div className="mb-[105px] xl:flex">
-          <Image
-            className="rounded-[20px] sm:mb-4 w-[357px] sm:h-[212px] md:w-[704px] md:h-[372px] xl:mr-[24px]"
-            src={image1}
-            alt="batch_cooking"
-            width={704}
-            height={372}
-          />
-          <Image
-            className="rounded-[20px] sm:w-[357px] sm:h-[212px] md:w-[704px] md:h-[372px]"
-            src={image2}
-            alt="delicious_food"
-            width={704}
-            height={372}
-          />
+          <div className="xl:mr-[24px]">
+            <Image
+              className="rounded-[20px] sm:mb-4 w-[357px] sm:h-[212px] md:w-[704px] md:h-[372px] "
+              src={image2}
+              alt="batch_cooking"
+              max-width={704}
+              max-height={372}
+            />
+            <div className=""></div>
+          </div>
+          <div className="relative">
+            <Image
+              className="rounded-[20px] sm:w-[357px] sm:h-[212px] md:w-[704px] md:h-[372px]"
+              src={image1}
+              alt="delicious_food"
+              max-width={704}
+              max-height={372}
+            />
+            <div className="absolute left-[21px] top-[50px] flex flex-col items-center md:left-[32px] md:top-[92px]">
+              <span className="mb-4 text-neutral-800 font-['Lora'] uppercase sm:text-[28px] font-bold md:text-[52px] md:font-semibold">
+                Тільки тут
+              </span>
+              <span className="text-center text-blue-700 text-base font-medium font-['Lato'] leading-relaxed md:text-[32px] ">
+                Висока якість!
+                <br />
+                Найкраща ціна!
+              </span>
+            </div>
+          </div>
         </div>
       </Container>
     </Section>
