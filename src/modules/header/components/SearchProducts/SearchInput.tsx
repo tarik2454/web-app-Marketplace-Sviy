b0dfb7fa36ba3ab77e057b49744b5af3a753dfe2
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-01-31 21:33:28 +0200

    changes in serch area

--- src/modules/header/components/SearchProducts/SearchInput.tsx
+++ src/modules/header/components/SearchProducts/SearchInput.tsx
@@ -6,7 +6,7 @@ export default function SearchInput () {
       <button className="text-blue-700">
         <SpriteSVG name="search" />
       </button>
-      <input className="w-full outline-none px-2 text-neutral-800 placeholder-neutral-800" type="text" placeholder="Я шукаю" />
+      <input className="w-full outline-none px-2 bg-neutral-50 text-neutral-800 placeholder-neutral-800" type="text" placeholder="Я шукаю" />
     </div>
   );
 }
