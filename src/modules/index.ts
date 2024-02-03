b7c12bc99b0db81de49744d9d6f3228537a80714
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-02-03 17:17:06 +0200

    trying more complex path

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,6 @@
 export { default as Header } from './header/Header';
 export { default as Footer } from './footer/Footer';
-export { default as AboutUs } from "./AboutUs/AboutUs";
+export { default as AboutUs } from "@/modules/aboutUs/AboutUs";
 export { default as HeroSection } from './HeroSection/HeroSection';
 export { default as Categories } from './header/components/Catalog/Catalog';
 export { default as Advertising } from './advertising/Advertising';
