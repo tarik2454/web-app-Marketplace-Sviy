38573b5d90713848ffbed66b5af1ff06a78bdafb
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-02-03 18:13:29 +0200

    trying fixing problem by deleting reexport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import ProductsWeek from '@/modules/productsWeek/ProductsWeek';
 import Advertising from '@/modules/advertising/Advertising';
-import { AboutUs, HeroSection } from '@/modules';
+import AboutUs from '@/modules/aboutUs/AboutUs';
+import { HeroSection } from '@/modules';
 import Novelties from '@/modules/novelties/Novelties';
 
 export default function Home() {
