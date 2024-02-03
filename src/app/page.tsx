0499aa5bb13173dd27a8d8991cf3617d57b6c750
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-02-03 20:01:04 +0200

    presave

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import ProductsWeek from '@/modules/productsWeek/ProductsWeek';
 import Advertising from '@/modules/advertising/Advertising';
-import AboutUs from '@/modules/aboutUs/AboutUs';
-import { HeroSection } from '@/modules';
+import { AboutUs, HeroSection } from '@/modules';
 import Novelties from '@/modules/novelties/Novelties';
 
 export default function Home() {
