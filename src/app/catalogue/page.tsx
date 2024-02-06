24fdd9d48e72c6d2625f35348088cb7ad03c672c
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-02-06 12:09:06 +0200

    start for catalogue page

---
+++ src/app/catalogue/page.tsx
@@ -0,0 +1,16 @@
+import Container from "@/shared/components/Container/Container";
+import Section from "@/shared/components/Section/Section";
+
+export default function Catalogue () {
+  return (
+    <>
+      <Section>
+        <Container>
+          <div>
+            
+          </div>
+        </Container>
+      </Section>
+    </>
+  )
+}
