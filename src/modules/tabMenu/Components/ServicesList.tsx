cd2718ecf93116383af0c2e80368cdfcf95eed6a
Taras Kovalyk <tarik2454@gmail.com>
2024-02-19 21:47:04 +0200

    adds style tab menu

--- src/modules/tabMenu/Components/ServicesList.tsx
+++ src/modules/tabMenu/Components/ServicesList.tsx
@@ -1,24 +1,26 @@
-import Link from 'next/link';
-
 export default function ServicesList() {
   return (
-    <div className="flex gap-6">
-      <div className="flex flex-col w-[628px]">
-        <p className="mb-6 text-2xl leading-[1.6] text-blue-90">Оплата</p>
+    <div className="flex flex-col md:flex-row gap-6 md:gap-[107px] xl:gap-6">
+      <div className="flex flex-col md:w-[343px] xl:w-[628px]">
+        <p className="mb-6 xl:mb-5 text-xl xl:text-2xl text-blue-90">Оплата</p>
         <ul className="flex flex-col gap-4">
-          <li className="text-xl">Оплата під час отримання товару</li>
-          <li className="text-xl">
+          <li className="text-xl md:text-base xl:text-xl">
+            Оплата під час отримання товару
+          </li>
+          <li className="text-xl md:text-base xl:text-xl">
             Оплата на карту ПриватБанку 5457 0822 2339 2551
           </li>
         </ul>
       </div>
 
-      <div className="flex flex-col w-[628px]">
-        <p className="mb-6 text-2xl leading-[1.6] text-blue-90">Доставка</p>
+      <div className="flex flex-col xl:w-[628px]">
+        <p className="mb-6 xl:mb-5 text-xl xl:text-2xl text-blue-90">
+          Доставка
+        </p>
         <ul className="flex flex-col gap-4">
-          <li className="text-xl">Нова Пошта</li>
-          <li className="text-xl">Укрпошта</li>
-          <li className="text-xl">Самовивіз</li>
+          <li className="text-xl md:text-base xl:text-xl">Нова Пошта</li>
+          <li className="text-xl md:text-base xl:text-xl">Укрпошта</li>
+          <li className="text-xl md:text-base xl:text-xl">Самовивіз</li>
         </ul>
       </div>
     </div>
--- src/shared/components/Pagination/Pagination.tsx
+++ src/shared/components/Pagination/Pagination.tsx
@@ -1,12 +1,7 @@
 'use client';
 
 import { SpriteSVG } from '@/shared/img/SpriteSVG';
-import React, {
-  MouseEventHandler,
-  ReactNode,
-  useEffect,
-  useState,
-} from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
 
 interface PaginationItem {
@@ -17,17 +12,35 @@ type PropsPagination<T> = {
   itemsPerPage: number;
   array: T[];
   renderItem: (item: T, index: number) => ReactNode;
-  styleMarginBottom?: string;
+  styleUl: string;
 };
 
 export default function Pagination<T extends PaginationItem>({
   itemsPerPage,
   array,
   renderItem,
-  styleMarginBottom,
+  styleUl,
 }: PropsPagination<T>) {
   const [currentPage, setCurrentPage] = useState(0);
   const [displayedItems, setDisplayedItems] = useState(itemsPerPage);
+
+  // const handleLoadMore = () => {
+  //   const nextPage = currentPage + 1; // Вычисляем номер следующей страницы
+  //   const nextPageEndIndex = nextPage * itemsPerPage; // Вычисляем индекс конца следующей страницы
+
+  //   // Если индекс конца следующей страницы больше или равен длине массива, значит достигнут конец данных и мы не должны увеличивать currentPage и displayedItems в этом случае
+  //   if (nextPageEndIndex >= array.length) {
+  //     return;
+  //   }
+
+  //   // Обновляем currentPage и displayedItems
+  //   setCurrentPage(nextPage);
+  //   setDisplayedItems(nextPageEndIndex);
+  // };
+
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + displayedItems;
+  const currentItems = array.slice(startIndex, endIndex);
 
   const handleLoadMore = () => {
     // Увеличиваем количество отображаемых элементов
@@ -42,27 +55,14 @@ export default function Pagination<T extends PaginationItem>({
     }
   };
 
-  useEffect(() => {
-    // Устанавливаем текущую страницу, основываясь на количестве отображаемых элементов
-    setCurrentPage(Math.ceil(displayedItems / itemsPerPage));
-  }, [displayedItems, itemsPerPage]);
-
   const handlePageClick = (selectedPage: { selected: number }) => {
     setCurrentPage(selectedPage.selected);
     setDisplayedItems(itemsPerPage);
   };
 
-  // const startIndex = currentPage * itemsPerPage;
-  // const endIndex = startIndex + itemsPerPage;
-  // const currentItems = array.slice(startIndex, endIndex);
-
-  const startIndex = currentPage * itemsPerPage;
-  const endIndex = startIndex + displayedItems;
-  const currentItems = array.slice(startIndex, endIndex);
-
   return (
     <div>
-      <ul className={styleMarginBottom}>
+      <ul className={styleUl}>
         {currentItems.map((item, index) => renderItem(item, index))}
       </ul>
 
@@ -73,14 +73,13 @@ export default function Pagination<T extends PaginationItem>({
         Дивитись більше
       </button>
 
-      {/* Пагинация */}
       <ReactPaginate
         className="flex justify-center items-center gap-5"
         breakLabel="..."
         nextLabel={<SpriteSVG name="pagination-rigth" />}
         nextClassName="flex p-[9px]"
         onPageChange={handlePageClick}
-        activeClassName="text-blue-900 text-red-500 text-xl"
+        activeClassName="text-blue-900"
         pageRangeDisplayed={3}
         marginPagesDisplayed={1}
         pageCount={Math.ceil(array.length / itemsPerPage)}
