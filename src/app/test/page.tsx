12e6e2ea7025a9fdc065c4799a7c4c24e4092a41
Taras Kovalyk <tarik2454@gmail.com>
2024-03-29 13:10:30 +0200

    feat: adds test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,24 @@
+import { Container } from '@/shared/components';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div className="h-screen pt-[150px]">
+      <Container>
+        <div className="flex flex-col">
+          <Link href="/">- main</Link>
+          <Link href="/cart">- cart</Link>
+          <Link href="/catalogue">- catalogue</Link>
+          <Link href="/catalogue-seller">- catalogue-seller</Link>
+          <Link href="/my-data">- my-data</Link>
+          <Link href="/orderDetails">- orderDetails</Link>
+          <Link href="/product-card">- product-card</Link>
+          <Link href="/recover-password">- recover-password</Link>
+          <Link href="/signin">- signin</Link>
+          <Link href="/signin-forgot">- signin-forgot</Link>
+          <Link href="/signup">- signup</Link>
+        </div>
+      </Container>
+    </div>
+  );
+}
