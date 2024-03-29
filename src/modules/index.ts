3d0254a732e4c6d09c093814a801f341c9ca80b2
Taras Kovalyk <tarik2454@gmail.com>
2024-03-29 12:39:05 +0200

    fix: renames the module

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,7 @@
 export { default as Header } from './header/Header';
 export { default as Footer } from './footer/Footer';
 export { default as AboutUs } from './aboutUs/AboutUs';
-export { default as HeroSection } from './HeroSection/HeroSection';
+export { default as HeroSection } from './heroSection/HeroSection';
 export { default as Categories } from './header/components/Catalog/Catalog';
 export { default as Advertising } from './advertising/Advertising';
 export { default as Filter } from './filter/Filter';
