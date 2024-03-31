afc8270f36df79e2266a83f5cda807ea1c9b73ae
LukeBrutale <yuriybema@gmail.com>
2024-03-31 21:03:39 +0300

    fixed styles

--- src/modules/myCart/MyCart.tsx
+++ src/modules/myCart/MyCart.tsx
@@ -2,7 +2,12 @@
 
 import CartList from '@/modules/header/components/Cart/CartList';
 
-import { Section, FormHeading, FormInput } from '@/shared/components';
+import {
+  Section,
+  FormHeading,
+  FormInput,
+  Container,
+} from '@/shared/components';
 import { FinalPrice } from '@/shared/components';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
@@ -23,44 +28,48 @@ export default function MyCart() {
   });
 
   return (
-    <Section className="relative pt-[3px] md:px-4 xl:px-[80px] xl:pt-[58px]">
-      <h2 className="px-4 pb-10 text-2xl leading-10 text-gray-900 md:pb-8 xl:text-4xl ">
-        Оформлення замовлення
-      </h2>
-      <h3 className="px-4 pb-5 text-xl leading-loose text-gray-900 xl:text-2xl">
-        Ваші контактні дані
-      </h3>
-      <form
-        className="flex flex-col min-w-[343px] mx-4 gap-5 "
-        onSubmit={formik.handleSubmit}
-      >
-        <div className="flex flex-col gap-5 md:w-[49%] xl:w-[65%]">
-          <FormInput
-            formik={formik}
-            id="name"
-            inputType="text"
-            inputIcon="user"
-            inputLink="Змінити"
-          />
-          <FormInput
-            formik={formik}
-            id="location"
-            inputType="text"
-            inputIcon="location"
-            inputLink="Змінити"
-          />
-          <div className="inline-flex items-center justify-start px-4 py-1.5 bg-white border rounded-2xl border-blue-700">
-            <p className="text-xs text-gray-900 md:text-base">
-              Зверніть увагу, товари, які замовлені у різних продавців, буде
-              доставлено окремими замовленнями.
-            </p>
+    <Section className="relative mb-10 pt-[3px] xl:pt-[58px]">
+      <Container>
+        <h1 className="sm:hidden font-lora text-3xl text-gray-900 md:block md:pb-[64px] xl:hidden">
+          Кошик
+        </h1>
+        <h2 className="pb-10 text-2xl leading-10 text-gray-900 md:pb-8 xl:text-4xl ">
+          Оформлення замовлення
+        </h2>
+        <h3 className="pb-5 text-xl leading-loose text-gray-900 xl:text-2xl">
+          Ваші контактні дані
+        </h3>
+        <form
+          className="flex flex-col min-w-[343px] gap-5 "
+          onSubmit={formik.handleSubmit}
+        >
+          <div className="flex flex-col gap-5 md:w-[49%] xl:w-[66%]">
+            <FormInput
+              formik={formik}
+              id="name"
+              inputType="text"
+              inputIcon="user"
+              inputLink="Змінити"
+            />
+            <FormInput
+              formik={formik}
+              id="location"
+              inputType="text"
+              inputIcon="location"
+              inputLink="Змінити"
+            />
+            <div className="inline-flex items-center justify-start px-4 py-1.5 bg-white border rounded-2xl border-blue-700">
+              <p className="text-xs text-gray-900 md:text-base">
+                Зверніть увагу, товари, які замовлені у різних продавців, буде
+                доставлено окремими замовленнями.
+              </p>
+            </div>
           </div>
-        </div>
-
-        <div className=" md:absolute md:top-[12%] md:right-8 xl:top-[15%] xl:right-[90px]">
-          <FinalPrice itemsQuantity={4} totalPrice={470} />
-        </div>
-      </form>
+          <div className="md:absolute md:top-[40%] md:right-[32px] xl:top-[37%] xl:right-[90px]">
+            <FinalPrice itemsQuantity={4} totalPrice={470} />
+          </div>
+        </form>
+      </Container>
     </Section>
   );
 }
