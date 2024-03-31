25f8337e49ee74bd462af5dac9e4b36f9b135138
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-03-31 11:05:09 +0300

    Update Container.tsx
    fix:fix mistake Container

--- src/shared/components/Container/Container.tsx
+++ src/shared/components/Container/Container.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from 'react';
 
 export default function Container({ children }: { children: ReactNode }) {
   return (
-    <div className="container mx-auto sm:px-4 md:px-8 xl:px-20">{children}</div>
+    <div className="container mx-auto px-4 md:px-8 xl:px-20">{children}</div>
   );
 }
