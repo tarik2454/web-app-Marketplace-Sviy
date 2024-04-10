403628d00c78d85312bf2a0fd9e2bf7f411f2cb8
Taras Kovalyk <tarik2454@gmail.com>
2024-04-10 15:48:36 +0300

    feet: adds styles wrapper to not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,10 +5,7 @@ export default function Page() {
   return (
     <PageWrapper>
       <ColorSpot />
-      <Breadcrumbs
-        containerClasses={'pt-5 mb-0 xl:pt-[26px]'}
-        homeElement={<span>Головна</span>}
-      />
+      <Breadcrumbs homeElement={<span>Головна</span>} />
       <NotFound />
     </PageWrapper>
   );
