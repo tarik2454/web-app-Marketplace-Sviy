360f5bb1be33e567d3bf98d388c1aeb9006c7461
alexandraru <115844615+alexandraru@users.noreply.github.com>
2024-04-13 18:06:52 +0300

    feat: add info about delivery and payment

--- src/app/delivery/page.tsx
+++ src/app/delivery/page.tsx
@@ -1,10 +1,23 @@
-import { Breadcrumbs, ColorSpot, PageWrapper } from '@/shared/components';
+import DeliveryInfo from '@/modules/infoComponents/DeliveryInfo';
+import PayInfo from '@/modules/infoComponents/PayInfo';
+import { Breadcrumbs, ColorSpot, Container, PageTitle, PageWrapper } from '@/shared/components';
 
 export default function Delivery() {
   return (
     <PageWrapper>
       <ColorSpot />
-      <Breadcrumbs homeElement={<span>Головна</span>} />
+      <Breadcrumbs 
+        containerClasses={'mb-[44px] md:mb-[98px] xl:mb-[98px]'}
+        homeElement={<span>Головна</span>} />
+      <Container>
+        <PageTitle name={'Доставка'} />
+      </Container>
+      <DeliveryInfo/>
+
+      <Container>
+        <PageTitle name={'Оплата'} />
+      </Container>
+      <PayInfo/>
     </PageWrapper>
   );
 }
---
+++ src/modules/infoComponents/PayInfo.tsx
@@ -0,0 +1,18 @@
+import { Container, Section } from '@/shared/components'
+import React from 'react'
+
+export default function PayInfo() {
+  return (
+    <Section className={
+        'pt-[20px] pb-[80px] md:pt-[40px] md:pb-[164px] xl:pt-[40px] xl:pb-[164px] xl:text-xl md:text-xl text-base'
+      }>
+      <Container>
+        <p className='mb-8'>В нашому маркетплейсі, продавець визначає доступні способи оплати замовлення. Ви можете обрати найбільш зручний для вас варіант при оформленні покупки:</p>
+        <ul className='!list-disc pl-8 text-pretty'>
+            <li className='!list-disc'>Оплата при отриманні: оплатіть готівкою при самовивозі з точки видачі продавця або у відділенні кур'єрської служби.</li>
+            <li className='!list-disc'>Оплата на картку продавця: здійсніть прямий банківський переказ на картку продавця згідно з наданими реквізитами.</li>
+        </ul>
+      </Container>
+    </Section>
+  )
+}
