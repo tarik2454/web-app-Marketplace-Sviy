857eb2eb3893320af4a492be5a8e67440d12abce
alexandraru <115844615+alexandraru@users.noreply.github.com>
2024-04-14 21:55:16 +0300

    bugfix: fix sections title

--- src/app/delivery/page.tsx
+++ src/app/delivery/page.tsx
@@ -1,6 +1,6 @@
 import DeliveryInfo from '@/modules/infoComponents/DeliveryInfo';
 import PayInfo from '@/modules/infoComponents/PayInfo';
-import { Breadcrumbs, ColorSpot, Container, PageTitle, PageWrapper } from '@/shared/components';
+import { Breadcrumbs, ColorSpot, Container, PageWrapper, SectionTitle } from '@/shared/components';
 
 export default function Delivery() {
   return (
@@ -10,12 +10,12 @@ export default function Delivery() {
         containerClasses={'mb-[44px] md:mb-[98px] xl:mb-[98px]'}
         homeElement={<span>Головна</span>} />
       <Container>
-        <PageTitle name={'Доставка'} />
+        <SectionTitle name={'Доставка'} className="mb-0 xl:mb-0 md:mb-0"/>
       </Container>
       <DeliveryInfo/>
 
       <Container>
-        <PageTitle name={'Оплата'} />
+        <SectionTitle name={'Оплата'} className="mb-0 xl:mb-0 md:mb-0"/>
       </Container>
       <PayInfo/>
     </PageWrapper>
