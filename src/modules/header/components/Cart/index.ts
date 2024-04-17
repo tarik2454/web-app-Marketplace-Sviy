1e15e63a7a75885406c9fbb65d4a89f3f1617646
Taras Kovalyk <tarik2454@gmail.com>
2024-04-17 20:07:56 +0300

    adds changes

--- src/modules/header/components/Cart/index.ts
+++ src/modules/header/components/Cart/index.ts
@@ -1,2 +1,2 @@
 export { default as FinalPrice } from '../../../../shared/components/OrderFinalPrice/OrderFinalPrice';
-export { default as AmountButtons } from './AmountButtons';
+export { default as AmountButtons } from '../../../../shared/components/AmountButtons/AmountButtons';
