4615aa6e0507563ea641b8fd87476d39ffda5c60
Taras Kovalyk <tarik2454@gmail.com>
2024-04-19 20:36:39 +0300

    adds chanes

--- src/app/delivery/page.tsx
+++ src/app/delivery/page.tsx
@@ -1,19 +1,30 @@
 import DeliveryInfo from '@/modules/infoComponents/DeliveryInfo';
 import PayInfo from '@/modules/infoComponents/PayInfo';
-import { Breadcrumbs, ColorSpot, Container, PageTitle, PageWrapper, SectionTitle } from '@/shared/components';
+import {
+  Breadcrumbs,
+  ColorSpot,
+  Container,
+  PageTitle,
+  PageWrapper,
+  SectionTitle,
+} from '@/shared/components';
 
 export default function Delivery() {
   return (
     <PageWrapper>
       <ColorSpot />
-      <Breadcrumbs 
-        containerClasses={'xl:mb-[98px] md:mb-[98px] mb-[44px]'}
-        homeElement={<span>Головна</span>} />
+      <Breadcrumbs
+        containerClasses={'mb-[44px] md:mb-[98px]'}
+        homeElement={<span>Головна</span>}
+      />
       <Container>
-      <PageTitle name={'Доставка та оплата '} stylesPageTitle='visually hidden text-[1px]'/>
+        <PageTitle
+          name={'Доставка та оплата'}
+          stylesPageTitle="visually-hidden"
+        />
       </Container>
-      <DeliveryInfo/>
-      <PayInfo/>
+      <DeliveryInfo />
+      <PayInfo />
     </PageWrapper>
   );
 }
