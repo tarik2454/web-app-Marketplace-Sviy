d1841a5f0b49e637da89d84e144daac4859dc286
LukeBrutale <yuriybema@gmail.com>
2024-04-24 20:09:00 +0300

    Added a phone number to the registration form

--- src/shared/components/FormInput/FormInput.tsx
+++ src/shared/components/FormInput/FormInput.tsx
@@ -5,16 +5,16 @@ import { FormikProps } from 'formik';
 import { useEffect, useState } from 'react';
 import styles from '@/styles/FormInput.module.css';
 import Link from 'next/link';
+import InputMask from 'react-input-mask';
 
 type Props = {
   name: string;
   label?: string;
-  inputType: 'password' | 'text' | 'email' | 'number' | 'textarea';
+  inputType?: 'password' | 'text' | 'email' | 'number' | 'textarea' | 'tel';
   formik: FormikProps<any>;
   placeholder?: string;
   inputIcon?: string;
   inputLink?: string;
-  // fieldName?: string;
 };
 
 export default function FormInput({
@@ -25,8 +25,7 @@ export default function FormInput({
   placeholder,
   inputIcon,
   inputLink,
-}: // fieldName,
-Props) {
+}: Props) {
   const [inputTypePass, setInputTypePass] = useState(inputType);
   const error = formik.touched[name] && formik.errors[name];
   const [borderColor, setBorderColor] = useState('border-blue-200');
@@ -49,7 +48,7 @@ Props) {
       <div
         className={`flex px-4 py-3 gap-2 relative ${borderColor} border-[1px] bg-white rounded-default`}
       >
-        {inputType !== 'textarea' && (
+        {inputType !== 'textarea' && inputType !== 'tel' && (
           <input
             onChange={formik.handleChange}
             type={inputTypePass}
@@ -67,6 +66,16 @@ Props) {
             placeholder={placeholder}
             value={formik.values[name]}
             className="w-full h-6 outline-none flex-grow order-2 overflow-hidden"
+          />
+        )}
+        {inputType === 'tel' && (
+          <InputMask
+            mask="+3\80 99 999 99 99"
+            maskChar="_"
+            placeholder="+380"
+            name={name}
+            onChange={formik.handleChange}
+            className="w-full h-6 outline-none p-invalid"
           />
         )}
         {inputIcon && (
