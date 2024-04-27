1ecbf12db18b4ffa30ae08f6e4f6c9080cb662f0
LukeBrutale <yuriybema@gmail.com>
2024-04-27 08:48:41 +0300

    add myOrder

--- src/modules/personalMyOrders/PersonalMyOrders.tsx
+++ src/modules/personalMyOrders/PersonalMyOrders.tsx
@@ -1,3 +1,12 @@
+'use client';
+import ScreenSize from '@/shared/hooks/useMediaQuery';
+import {
+  Container,
+  PersonalDynamicSidebar,
+  Section,
+} from '@/shared/components';
+
 export default function PersonalMyOrders() {
+  const { isOnMobile, isOnTablet } = ScreenSize();
   return <div>Root</div>;
 }
