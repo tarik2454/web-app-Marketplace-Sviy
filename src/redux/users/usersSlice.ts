ac82fc2a10a7b8c23c5bb1991b795550f1cea146
Taras Kovalyk <tarik2454@gmail.com>
2024-04-27 22:48:48 +0300

    feet: adds test slice to redux

---
+++ src/redux/users/usersSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { fetchUsersThunk } from './operations';
+import { RootState } from '../store';
+
+interface UsersState {
+  entities: any;
+  isLoading: boolean;
+  value: number;
+}
+
+const initialState: UsersState = {
+  entities: [],
+  isLoading: false,
+  value: 10,
+};
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+
+  reducers: {
+    increment: state => {
+      state.value = state.value + 1;
+    },
+    decrement: state => {
+      state.value = state.value - 1;
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(fetchUsersThunk.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.entities.push(...action.payload);
+    });
+    builder.addCase(fetchUsersThunk.pending, (state, action) => {
+      state.isLoading = true;
+    });
+  },
+});
+
+export const { increment, decrement } = usersSlice.actions;
+
+export const selectUsers = (state: RootState) => state.users;
+
+export default usersSlice.reducer;
