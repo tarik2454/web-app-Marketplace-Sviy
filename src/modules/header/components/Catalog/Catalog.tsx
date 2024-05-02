7e8dc5be8fded9c9dc4899d21769ca090561a6c4
Taras Kovalyk <tarik2454@gmail.com>
2024-05-02 21:04:38 +0300

    fix: fix bug catalog open

--- src/modules/header/components/Catalog/Catalog.tsx
+++ src/modules/header/components/Catalog/Catalog.tsx
@@ -25,6 +25,8 @@ export default function Catalog({
     setSelectedCategory(categoryName);
   };
 
+  console.log(isThirdList);
+
   const stylesCatalog = `w-full md:w-[704px] xl:w-[1280px] h-full md:h-[80vh] my-0 mx-auto bg-neutral-50 md:rounded-br-default md:rounded-bl-default shadow-[2px_2px_12px_0_rgba(186,186,186,0.40)] absolute top-0 md:top-[113px] left-[50%] z-20 -translate-x-2/4`;
 
   return (
