99bf1092987a389755996a7761e17f57ad795c3b
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-05-10 17:56:11 +0300

    feat: add PersonalMyAdd module

--- src/modules/personalMyAds/PersonalMyAds.tsx
+++ src/modules/personalMyAds/PersonalMyAds.tsx
@@ -1,3 +1,40 @@
+'use client';
+import ScreenSize from '@/shared/hooks/useMediaQuery';
+import {
+  Container,
+  PersonalDynamicSidebar,
+  Section,
+} from '@/shared/components';
+import PersonalMyAdsItems from './components/PersonalMyAddItems';
+
+
 export default function PersonalMyAds() {
-  return <div>Root</div>;
+  const { isOnMobile, isOnTablet } = ScreenSize();
+  return (
+    <Section className="pt-0 xl:pt-0 md:pt-0 pb-[80px] md:pb-[104px] xl:pb-[164px]">
+      <Container>
+        {(isOnMobile && (
+          <PersonalDynamicSidebar
+            activeAds="active"
+            childAds={<PersonalMyAdsItems />}
+          />
+        )) ||
+          (isOnTablet && (
+            <PersonalDynamicSidebar
+              activeAds="active"
+              childAds={<PersonalMyAdsItems />}
+            />
+          )) || (
+            <div className="flex items-start gap-6">
+              <div className="basis-1/3 max-w-[302px]">
+                <PersonalDynamicSidebar childAds="active" />
+              </div>
+              <div className="basis-3/4">
+                <PersonalMyAdsItems />
+              </div>
+            </div>
+          )}
+      </Container>
+    </Section>
+  );
 }
