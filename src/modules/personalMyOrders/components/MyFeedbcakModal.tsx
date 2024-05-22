e99374a6dc0b937ba0a23c71aa2a83ce703629e3
LukeBrutale <yuriybema@gmail.com>
2024-05-22 16:16:39 +0300

    added modal

--- src/modules/personalMyOrders/components/MyFeedbcakModal.tsx
+++ src/modules/personalMyOrders/components/MyFeedbcakModal.tsx
@@ -14,13 +14,14 @@ export default function MyFeedbackModal({
     <>
       {isOpenModal && (
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-          <div className="bg-white px-5 py-6 rounded-2xl sm:min-w-[343px]">
+          <div className="bg-white px-5 py-6 rounded-2xl w-full mx-5 md:mx-10 md:px-10 md:py-15 xl:max-w-[768px] ">
             <div className="flex justify-between">
-              <h2 className="text-2xl leading-10 text-gray-900 mb-4">
+              <h2 className="text-2xl leading-10 text-gray-900 ">
                 Написати відгук
               </h2>
               <CloseButton closeButtonClick={handleCloseModal} />
             </div>
+            <div className="border-b-2 border-gray-300 w-full mb-5"></div>
 
             <p className="text-base leading-relaxed text-gray-900 mb-5">
               Ваша оцінка товара
@@ -49,8 +50,8 @@ export default function MyFeedbackModal({
               className="w-full p-2 border rounded-2xl mb-4"
               rows={5}
             ></textarea>
-            <div className="flex flex-col items-center">
-              <OrangeButton cssSettings="px-8 py-2.5 font-medium text-white mb-4">
+            <div className="flex flex-col items-center md:flex-row md:justify-center">
+              <OrangeButton cssSettings="px-8 py-2.5 font-medium text-white mb-4 md:mr-6">
                 Залишити відгук
               </OrangeButton>
               <ArrowButton
