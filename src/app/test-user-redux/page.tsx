95bab510f3179ed454c9ab5a3d824ef8e1ae6ae1
Taras Kovalyk <tarik2454@gmail.com>
2024-06-13 22:17:06 +0300

    fix bug deploy

--- src/app/test-user-redux/page.tsx
+++ src/app/test-user-redux/page.tsx
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 
 // import { useEffect, useRef, useState } from 'react';
 
@@ -9,87 +9,87 @@
 
 // import { User } from '@/types/interfaces/User';
 
-// import {
-//   Container,
-//   PageTitle,
-//   PageWrapper,
-//   Section,
-// } from '@/shared/components';
+import {
+  Container,
+  PageTitle,
+  PageWrapper,
+  Section,
+} from '@/shared/components';
 
-// export default function Home() {
-//   const [isHydrated, setIsHydrated] = useState(false);
+export default function Home() {
+  // const [isHydrated, setIsHydrated] = useState(false);
 
-//   const { entities, isLoading, value } = useAppSelector(selectUsers);
-//   const isAuthState = useAppSelector(selectAuth);
+  // const { entities, isLoading, value } = useAppSelector(selectUsers);
+  // const isAuthState = useAppSelector(selectAuth);
 
-//   const userRef = useRef(false);
-//   const dispatch = useAppDispatch();
+  // const userRef = useRef(false);
+  // const dispatch = useAppDispatch();
 
-//   useEffect(() => {
-//     if (userRef.current === false) {
-//       dispatch(fetchUsersThunk());
-//     }
-//     return () => {
-//       userRef.current = true;
-//     };
-//   }, [dispatch]);
+  // useEffect(() => {
+  //   if (userRef.current === false) {
+  //     dispatch(fetchUsersThunk());
+  //   }
+  //   return () => {
+  //     userRef.current = true;
+  //   };
+  // }, [dispatch]);
 
-//   useEffect(() => {
-//     setIsHydrated(true);
-//   }, []);
+  // useEffect(() => {
+  //   setIsHydrated(true);
+  // }, []);
 
-//   return (
-//     <PageWrapper>
-//       <Section className={''}>
-//         <Container>
-//           <PageTitle stylesPageTitle="mb-5" name="Test user redux" />
-//           {isLoading ? (
-//             <h1>Loading...</h1>
-//           ) : (
-//             <>
-//               {entities?.map((user: User) => (
-//                 <p key={user.id}>{user.name}</p>
-//               ))}
+  return (
+    <PageWrapper>
+      {/* <Section className={''}>
+        <Container>
+          <PageTitle stylesPageTitle="mb-5" name="Test user redux" />
+          {isLoading ? (
+            <h1>Loading...</h1>
+          ) : (
+            <>
+              {entities?.map((user: User) => (
+                <p key={user.id}>{user.name}</p>
+              ))}
 
-//               <div className="flex gap-3">
-//                 <button onClick={() => dispatch(decrement())}>-</button>
-//                 {isHydrated ? value : null}
-//                 <button onClick={() => dispatch(increment())}>+</button>
-//               </div>
-//             </>
-//           )}
-//         </Container>
-//       </Section>
+              <div className="flex gap-3">
+                <button onClick={() => dispatch(decrement())}>-</button>
+                {isHydrated ? value : null}
+                <button onClick={() => dispatch(increment())}>+</button>
+              </div>
+            </>
+          )}
+        </Container>
+      </Section> */}
 
-//       <Section className={'pb-20'}>
-//         <Container>
-//           <PageTitle stylesPageTitle="mb-5" name="Test user redux" />
-//           <div className="grid md:grid-cols-2 place-items-center">
-//             <div className="flex gap-4 border border-1 border-black p-20">
-//               <button
-//                 className="p-4 border border-1 border-black hover:bg-gray-300"
-//                 onClick={() => dispatch(setAuthState(true))}
-//               >
-//                 Log in
-//               </button>
-//               <button
-//                 className="p-4 border border-1 border-black hover:bg-gray-300"
-//                 onClick={() => dispatch(setAuthState(false))}
-//               >
-//                 Log out
-//               </button>
-//             </div>
+      <Section className={'pb-20'}>
+        <Container>
+          <PageTitle stylesPageTitle="mb-5" name="Test user redux" />
+          <div className="grid md:grid-cols-2 place-items-center">
+            <div className="flex gap-4 border border-1 border-black p-20">
+              <button
+                className="p-4 border border-1 border-black hover:bg-gray-300"
+                // onClick={() => dispatch(setAuthState(true))}
+              >
+                Log in
+              </button>
+              <button
+                className="p-4 border border-1 border-black hover:bg-gray-300"
+                // onClick={() => dispatch(setAuthState(false))}
+              >
+                Log out
+              </button>
+            </div>
 
-//             <div className="flex gap border border-1 border-black p-20">
-//               {isHydrated && (
-//                 <div>
-//                   You are now {isAuthState ? 'Logged In' : 'Logged Out'}
-//                 </div>
-//               )}
-//             </div>
-//           </div>
-//         </Container>
-//       </Section>
-//     </PageWrapper>
-//   );
-// }
+            {/* <div className="flex gap border border-1 border-black p-20">
+              {isHydrated && (
+                <div>
+                  You are now {isAuthState ? 'Logged In' : 'Logged Out'}
+                </div>
+              )}
+            </div> */}
+          </div>
+        </Container>
+      </Section>
+    </PageWrapper>
+  );
+}
