5d43a070d94f7d93b3aca696c31c21afd2375f32
Taras Kovalyk <tarik2454@gmail.com>
2024-06-28 18:59:26 +0300

    feet: adds changes base url

--- src/config/globalConfig.ts
+++ src/config/globalConfig.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 // Define a type for  API
 export const API: AxiosInstance = axios.create({
-  baseURL: 'http://ec2-13-51-150-87.eu-north-1.compute.amazonaws.com',
+  baseURL: 'http://localhost',
 });
 
 // `token` parameter is typed as a string
