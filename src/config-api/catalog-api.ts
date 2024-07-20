0209bfd2297a46f06c71b3910dd52a700185741a
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-07-20 15:09:06 +0300

    refctor: fix NewAds

--- src/config-api/catalog-api.ts
+++ src/config-api/catalog-api.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { APIEXTRA } from './global-config-api';
 
 export const fetchCatalog = async () => {
@@ -5,6 +6,10 @@ export const fetchCatalog = async () => {
     const data = await APIEXTRA.get('/api/catalog/category/');
     return data.data.results;
   } catch (error) {
-    console.log('Error fetching catalog data:', error);
+    if (axios.isAxiosError(error)) {
+      console.warn('Axios error fetching catalog data', error.message);
+    } else {
+      console.warn('Unknown error fetching catalog data', error);
+    }
   }
 };
