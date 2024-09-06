7c606bb8fe529f60ebd10d810a07e04d542a059d
LukeBrutale <yuriybema@gmail.com>
2024-09-06 14:19:12 +0300

    added advertsSlice v2.0

--- src/redux/adverts/advertsSlice.ts
+++ src/redux/adverts/advertsSlice.ts
@@ -1,9 +1,11 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { createAdvertThunk,  
-  getAdvertListThunk, 
-  getAdvertThunk, 
-  updateAdvertThunk, 
-  deleteAdvertThunk  } from './operations';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import {
+  createAdvertThunk,
+  getAdvertListThunk,
+  getAdvertThunk,
+  updateAdvertThunk,
+  deleteAdvertThunk,
+} from './operations';
 import { RootState } from '../store';
 
 interface Address {
@@ -48,84 +50,65 @@ const advertsSlice = createSlice({
   reducers: {},
   extraReducers: builder => {
     builder
-    .addCase(
-      createAdvertThunk.fulfilled,
-      (state, { payload }: PayloadAction<AdvertResponse>) => {
+      .addCase(createAdvertThunk.fulfilled, (state, { payload }) => {
         state.entities.push(payload);
         state.isLoading = false;
-        state.error = null;
-      }
-    )
-    .addCase(createAdvertThunk.pending, state => {
-      state.isLoading = true;
-    })
-    .addCase(
-      createAdvertThunk.rejected,
-      (state, { payload }: PayloadAction<string | undefined>) => {
+        state.error = '';
+      })
+      .addCase(getAdvertListThunk.fulfilled, (state, { payload }) => {
+        state.entities = payload;
         state.isLoading = false;
-        state.error = payload || 'Не вдалося створити оголошення';
-      }
-    )
-  
-    .addCase(getAdvertListThunk.fulfilled, (state, { payload }: PayloadAction<AdvertResponse[]>) => {
-      state.entities = payload;
-      state.isLoading = false;
-      state.error = null;
-    })
-    .addCase(getAdvertListThunk.pending, state => {
-      state.isLoading = true;
-    })
-    .addCase(getAdvertListThunk.rejected, (state, { payload }: PayloadAction<string | undefined>) => {
-      state.isLoading = false;
-      state.error = payload || 'Не вдалося створити оголошення';
-    })
-
-    .addCase(getAdvertThunk.fulfilled, (state, { payload }: PayloadAction<AdvertResponse>) => {
-      const index = state.entities.findIndex(ad => ad.id === payload.id);
-      if (index !== -1) {
-        state.entities[index] = payload;
-      } else {
-        state.entities.push(payload);
-      }
-      state.isLoading = false;
-      state.error = null;
-    })
-    .addCase(getAdvertThunk.pending, state => {
-      state.isLoading = true;
-    })
-    .addCase(getAdvertThunk.rejected, (state, { payload }: PayloadAction<string | undefined>) => {
-      state.isLoading = false;
-      state.error = payload || 'Не вдалося створити оголошення';
-    })
-
-    .addCase(updateAdvertThunk.fulfilled, (state, { payload }: PayloadAction<AdvertResponse>) => {
-      const index = state.entities.findIndex(ad => ad.id === payload.id);
-      if (index !== -1) {
-        state.entities[index] = payload;
-      }
-      state.isLoading = false;
-      state.error = null;
-    })
-    .addCase(updateAdvertThunk.pending, state => {
-      state.isLoading = true;
-    })
-    .addCase(updateAdvertThunk.rejected, (state, { payload }: PayloadAction<string | undefined>) => {
-      state.isLoading = false;
-      state.error = payload || 'Не вдалося створити оголошення';
-    })
-
-    .addCase(deleteAdvertThunk.fulfilled, (state, { payload }: PayloadAction<number>) => {
-      state.entities = state.entities.filter(ad => ad.id !== payload);
-      state.isLoading = false;
-      state.error = null;
-    })
-    .addCase(deleteAdvertThunk.pending, state => {
-      state.isLoading = true;
-    })
-    .addCase(deleteAdvertThunk.rejected, (state, { payload }: PayloadAction<string | undefined>) => {
-      state.isLoading = false;
-      state.error = payload || 'Не вдалося створити оголошення';
-    });
+        state.error = '';
+      })
+      .addCase(getAdvertThunk.fulfilled, (state, { payload }) => {
+        const index = state.entities.findIndex(ad => ad.id === payload.id);
+        if (index !== -1) {
+          state.entities[index] = payload;
+        } else {
+          state.entities.push(payload);
+        }
+        state.isLoading = false;
+        state.error = '';
+      })
+      .addCase(updateAdvertThunk.fulfilled, (state, { payload }) => {
+        const index = state.entities.findIndex(ad => ad.id === payload.id);
+        if (index !== -1) {
+          state.entities[index] = payload;
+        }
+        state.isLoading = false;
+        state.error = '';
+      })
+      .addCase(deleteAdvertThunk.fulfilled, (state, { payload }) => {
+        state.entities = state.entities.filter(ad => ad.id !== payload);
+        state.isLoading = false;
+        state.error = '';
+      })
+      .addMatcher(
+        isAnyOf(
+          createAdvertThunk.pending,
+          getAdvertListThunk.pending,
+          getAdvertThunk.pending,
+          updateAdvertThunk.pending,
+          deleteAdvertThunk.pending
+        ),
+        state => {
+          state.isLoading = true;
+          state.error = '';
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          createAdvertThunk.rejected,
+          getAdvertListThunk.rejected,
+          getAdvertThunk.rejected,
+          updateAdvertThunk.rejected,
+          deleteAdvertThunk.rejected
+        ),
+        (state, { payload }) => {
+          state.isLoading = false;
+          state.error = payload ?? 'Помилка оновлення даних';
+        }
+      );
   },
 });
 
