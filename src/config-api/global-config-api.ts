561ecd04c71a6e47ef7e87ca944728f5b15f0ca5
Taras Kovalyk <tarik2454@gmail.com>
2024-09-20 22:12:33 +0300

    adds changes

--- src/config-api/global-config-api.ts
+++ src/config-api/global-config-api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 // Define a type for  API
 export const API: AxiosInstance = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/http://13.60.194.15:80',
+  baseURL: 'http://13.60.194.15:80',
 });
 
 // `token` parameter is typed as a string
