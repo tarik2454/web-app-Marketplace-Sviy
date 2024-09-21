3bf6052ae4e7930c35620b2b176b9915edc26bda
Taras Kovalyk <tarik2454@gmail.com>
2024-09-21 19:43:32 +0300

    adds changes

--- src/config-api/global-config-api.ts
+++ src/config-api/global-config-api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 // Define a type for  API
 export const API: AxiosInstance = axios.create({
-  baseURL: 'http://13.60.194.15:80',
+  baseURL: 'http://localhost',
 });
 
 // `token` parameter is typed as a string
