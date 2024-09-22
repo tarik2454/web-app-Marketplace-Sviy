3b8c42410652dff3d82b60be8d6c97985162999f
Taras Kovalyk <tarik2454@gmail.com>
2024-09-22 21:29:04 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     domains: ['png.pngtree.com', 'swiperjs.com'],
   },
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', 
+        destination: 'http://13.60.194.15/:path*', 
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
